1fe2999c1f661c5f55ce2ed53a90d641691f0a6c
Tiago Cruz <tiagoarrudadacruz@gmail.com>
2020-10-12 18:28:57 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/tiagoarruda/course/repositories/OrderItemRepository.java
+++ src/main/java/com/tiagoarruda/course/repositories/OrderItemRepository.java
@@ -1,7 +1,8 @@
 package com.tiagoarruda.course.repositories;
 
 import com.tiagoarruda.course.entities.OrderItem;
+import com.tiagoarruda.course.entities.pk.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 }
