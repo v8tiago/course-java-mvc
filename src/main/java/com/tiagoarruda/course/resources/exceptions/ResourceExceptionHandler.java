5d12bfb19db0bf2b77fc82be1f93ba6116ef9147
Tiago Cruz <tiagoarrudadacruz@gmail.com>
2020-10-13 18:51:35 -0300

    Exception handling - delete

--- src/main/java/com/tiagoarruda/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/tiagoarruda/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.tiagoarruda.course.resources.exceptions;
 
+import com.tiagoarruda.course.services.exceptions.DatabaseException;
 import com.tiagoarruda.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,16 @@ public class ResourceExceptionHandler {
     {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request)
+    {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(status).body(err);
--- src/main/java/com/tiagoarruda/course/services/UserService.java
+++ src/main/java/com/tiagoarruda/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.tiagoarruda.course.services;
 
 import com.tiagoarruda.course.entities.User;
 import com.tiagoarruda.course.repositories.UserRepository;
+import com.tiagoarruda.course.services.exceptions.DatabaseException;
 import com.tiagoarruda.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,7 +36,17 @@ public class UserService {
 
     public void delete(Long id)
     {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch(EmptyResultDataAccessException e)
+        {
+            throw new ResourceNotFoundException(id);
+        }
+        catch(DataIntegrityViolationException e)
+        {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj)
